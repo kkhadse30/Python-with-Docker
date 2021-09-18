b1f186aad91a1111509d2aa6eb4864b8f5a7ad86
kkhadse30 <75018574+kkhadse30@users.noreply.github.com>
2021-09-18 21:33:41 +0530

    Add files via upload

---
+++ assighnment.py
@@ -0,0 +1,20 @@
+answer1 = input("Quiz sport \nQuestion: Which one is correct team name in NBA? \nOptions: \n[ New York Bulls\n Los Angeles Kings \n Golden State Warriros \n Huston Rocket] \nAnswer:")
+if  answer1 == "Huston Rocket":
+    print ("correct")
+else:
+     print ("Incorrect! The answer is Huston Rocket. ")
+
+
+
+answer2 = input("Maths \nq1: \nquestion: 5 + 7 = ? \noptions: \n[ 10 \n 11 \n 12 \n 13 \nAnswer:")
+if  answer2 == "12":
+    print ("correct")
+else:
+     print ("Incorrect! The answer is 12. ")
+
+answer3 = input("Maths \nq1: \nquestion: 12 - 8 = ? \noptions: \n[ 1 \n 2 \n 3 \n 4 \nAnswer:")
+if  answer3 == "4":
+    print ("correct")
+else:
+     print ("Incorrect! The answer is 4. ")
+
